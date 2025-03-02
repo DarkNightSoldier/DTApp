0f3be0998df20b8a7590f805336ae881806923a3
Manuel Valle <103595751+mfvalle@users.noreply.github.com>
2025-03-02 14:20:35 -0500

    Estructura de datos del plan de estudio actual

---
+++ plan_estudios_actual.py
@@ -0,0 +1,120 @@
+# plan_estudios_actual.py
+
+components = {
+    "Fundamentación": {
+        "agrupaciones": [
+            {
+                "nombre": "Matemáticas y Estructuras Discretas",
+                "subjects": [
+                    ["2015168", "Fundamentos de matemáticas", 4],
+                    ["2015181", "Sistemas numéricos", 4],
+                    ["2025819", "Introducción a la teoría de conjuntos", 4],
+                    ["2015555", "Álgebra lineal básica", 4],
+                    ["2016377", "Cálculo diferencial en una variable", 4],
+                    ["2015536", "Cálculo integral en una variable", 4],
+                    ["2015612", "Cálculo vectorial", 4],
+                    ["2016342", "Cálculo de ecuaciones diferenciales", 4],
+                    ["2015178", "Probabilidad", 4],
+                    ["2015152", "Grupos y anillos", 4],
+                    ["2015184", "Teoría de grafos", 4],
+                    ["2017322", "Introducción al análisis combinatorio", 4],
+                    ["2028388", "Cadenas de Markov y aplicaciones", 4]
+                ]
+            },
+            {
+                "nombre": "Algoritmos y Teoría de la Computación",
+                "subjects": [
+                    ["2016696", "Algoritmos", 3],
+                    ["2015714", "Introducción a la teoría de la computación", 4],
+                    ["2026555", "Álgebra abstracta y computacional", 4],
+                    ["2012672", "Teoría de la recursión", 4],
+                    ["2027628", "Teoría de lenguajes formales", 4],
+                    ["2027629", "Lógica computacional", 4],
+                    ["2028641", "Quantum computer programming", 4],
+                    ["2029273", "Advanced data structures", 4],
+                    ["2029275", "Complejidad computacional", 4]
+                ]
+            },
+            {
+                "nombre": "Computación Científica",
+                "subjects": [
+                    ["2019702", "Análisis numérico I", 4],
+                    ["2015173", "Introducción a la optimización", 4],
+                    ["2019082", "Modelos matemáticos I", 4],
+                    ["2019701", "Análisis numérico II", 4],
+                    ["2026371", "Métodos numéricos en finanzas", 4],
+                    ["2026719", "Ecuaciones en diferencias finitas y sistemas dinámicos", 4]
+                ]
+            }
+        ]
+    },
+    "Disciplinar": {
+        "agrupaciones": [
+            {
+                "nombre": "Ciencias Naturales y Estadística",
+                "subjects": [
+                    ["1000101", "Biología molecular y celular", 3],
+                    ["1000010", "Probabilidad y estadística fundamental", 3],
+                    ["1000013", "Química básica", 4],
+                    ["2015017", "Mecánica newtoniana", 3],
+                    ["2016611", "Fundamentos de física teórica", 3],
+                    ["2022689", "Fundamentos de física", 3]
+                ]
+            },
+            {
+                "nombre": "Seguridad Informática y Codificación",
+                "subjects": [
+                    ["2027311", "Introducción a la criptografía y a la teoría de información", 4],
+                    ["2027310", "Criptografía", 3],
+                    ["2027313", "Teoría de la codificación", 4]
+                ]
+            },
+            {
+                "nombre": "Sistemas Operativos, de Cómputo y Compiladores",
+                "subjects": [
+                    ["2016698", "Elementos de computadores", 3],
+                    ["2016707", "Sistemas operativos", 3],
+                    ["2016697", "Arquitectura de computadores", 3],
+                    ["2016722", "Computación paralela y distribuida", 3],
+                    ["2025966", "Lenguajes de programación", 3],
+                    ["2027642", "Compiladores", 3]
+                ]
+            },
+            {
+                "nombre": "Programación y Estructuras de Datos",
+                "subjects": [
+                    ["2026573", "Introducción a las ciencias de la computación y a la programación", 3],
+                    ["2016375", "Programación orientada a objetos", 3],
+                    ["2016699", "Estructuras de datos", 3]
+                ]
+            },
+            {
+                "nombre": "Computación Aplicada",
+                "subjects": [
+                    ["2016353", "Bases de datos", 3],
+                    ["2016701", "Ingeniería de software I", 3],
+                    ["2025196", "Introducción a la biología computacional", 4],
+                    ["2025960", "Computación visual", 3],
+                    ["2025967", "Redes de computadores", 3],
+                    ["2025995", "Introducción a los sistemas inteligentes", 3],
+                    ["2027309", "Análisis forense digital", 4],
+                    ["2027631", "Introducción a la inteligencia artificial", 3],
+                    ["2027632", "Física computacional", 4],
+                    ["2027641", "Análisis de bases de datos", 3],
+                    ["2028837", "Matemáticas del aprendizaje de máquinas", 4],
+                    ["2028839", "Fundamentos de analítica de datos y aplicaciones", 4],
+                    ["2029900", "Redes neuronales, arquitecturas y aplicaciones", 4],
+                    ["2029272", "Práctica Profesional", 6]
+                ]
+            },
+            {
+                "nombre": "Trabajo de Grado",
+                "subjects": [
+                    ["2027633", "Trabajo de Grado - Trabajos investigativos", 8],
+                    ["2027634", "Trabajo de Grado - Asignaturas de posgrado", 8],
+                    ["2027636", "Trabajo de Grado - Pasantías", 8]
+                ]
+            }
+        ]
+    }
+}
